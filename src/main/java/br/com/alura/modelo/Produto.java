87c6ef9822d316a8f14439ac8e26df9e57d2fb43
AliceMeneses <alicesilvademeneses@gmail.com>
2021-04-24 21:16:24 -0300

    Adicionar construtor com argumentos e um construtor padrão para o hibernate

--- src/main/java/br/com/alura/modelo/Produto.java
+++ src/main/java/br/com/alura/modelo/Produto.java
@@ -19,6 +19,16 @@ public class Produto {
 	private String nome;
 	private String descricao;
 	private BigDecimal preco;
+	
+	public Produto() {
+		
+	}
+	
+	public Produto(String nome, String descricao, BigDecimal preco) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+	}
 
 	public Long getId() {
 		return id;
