b502330c80a89a3217a364d64e29e9e0f62b1f39
AliceMeneses <alicesilvademeneses@gmail.com>
2021-05-01 20:05:03 -0300

    Executar busca por nome do produto, mas capturar o preco do produto

--- src/main/java/br/com/alura/testes/OperacoesProduto.java
+++ src/main/java/br/com/alura/testes/OperacoesProduto.java
@@ -17,76 +17,89 @@ public class OperacoesProduto {
 
 		cadastrarProduto();
 		
-		buscarProduto();
+		buscarProdutoPorId(1L);
 		
 		buscarTodosProdutos();
 		
 		buscarProdutoPorNome("Zenfone 5");
 		
 		buscarProdutoPorNomeDaCategoria("CELULARES");
+		
+		buscarPrecoDoProdutoPorNome("Zenfone 5");
+		
 	}
 	
+	private static void buscarPrecoDoProdutoPorNome(String nome) {
+		
+		EntityManager entityManager = JPAUtil.getEntityManager();
+
+		ProdutoDao produtoDao = new ProdutoDao(entityManager);
+
+		BigDecimal preco = produtoDao.buscarPrecoDoProdutoPorNome(nome);
+
+		System.out.println("Preço do produto: " + preco);
+	}
+
 	private static void buscarProdutoPorNomeDaCategoria(String nome) {
-		
+
 		EntityManager entityManager = JPAUtil.getEntityManager();
-		
+
 		ProdutoDao produtoDao = new ProdutoDao(entityManager);
-		
+
 		List<Produto> produtos = produtoDao.buscarPorNomeDaCategoria(nome);
-		
-		produtos.forEach(System.out::println);
-	}
-	
-	private static void buscarProdutoPorNome(String nome) {
-		
-		
-		EntityManager entityManager = JPAUtil.getEntityManager();
-		
-		ProdutoDao produtoDao = new ProdutoDao(entityManager);
-		
-		List<Produto> produtos = produtoDao.buscarPorNome(nome);
-		
-		produtos.forEach(System.out::println);
-		
-	}
-	
-	private static void buscarTodosProdutos() {
-		
-		EntityManager entityManager = JPAUtil.getEntityManager();
-		
-		ProdutoDao produtoDao = new ProdutoDao(entityManager);
-				
-		List<Produto> produtos = produtoDao.buscarTodos();
-		
+
 		produtos.forEach(System.out::println);
 	}
 
-	private static void buscarProduto() {
-		
+	private static void buscarProdutoPorNome(String nome) {
+
 		EntityManager entityManager = JPAUtil.getEntityManager();
-				
+
 		ProdutoDao produtoDao = new ProdutoDao(entityManager);
-				
-		Produto produto = produtoDao.buscarPorId(1L);
-		
+
+		List<Produto> produtos = produtoDao.buscarPorNome(nome);
+
+		produtos.forEach(System.out::println);
+
+	}
+
+	private static void buscarTodosProdutos() {
+
+		EntityManager entityManager = JPAUtil.getEntityManager();
+
+		ProdutoDao produtoDao = new ProdutoDao(entityManager);
+
+		List<Produto> produtos = produtoDao.buscarTodos();
+
+		produtos.forEach(System.out::println);
+	}
+
+	private static void buscarProdutoPorId(Long id) {
+
+		EntityManager entityManager = JPAUtil.getEntityManager();
+
+		ProdutoDao produtoDao = new ProdutoDao(entityManager);
+
+		Produto produto = produtoDao.buscarPorId(id);
+
 		System.out.println(produto);
 	}
 
 	private static void cadastrarProduto() {
 		EntityManager entityManager = JPAUtil.getEntityManager();
-		
+
 		Categoria celulares = new Categoria("CELULARES");
-		
+
 		Produto celular = new Produto("Zenfone 5", "Cor preta", new BigDecimal(1000), celulares);
-		
+
 		ProdutoDao produtoDao = new ProdutoDao(entityManager);
 		CategoriaDao categoriaDao = new CategoriaDao(entityManager);
-		
+
 		entityManager.getTransaction().begin();
 		categoriaDao.cadastrar(celulares);
 		produtoDao.cadastrar(celular);
 		entityManager.getTransaction().commit();
-		
+
 		entityManager.close();
 	}
 
