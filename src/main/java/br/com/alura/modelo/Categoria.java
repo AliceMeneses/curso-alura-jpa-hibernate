fe42c65a6663e7330995cb3e82c1590ef298ee01
AliceMeneses <alicesilvademeneses@gmail.com>
2021-05-01 14:39:07 -0300

    Mudar enum Categoria para classe, para permitir que usuários criem uma quantidade infinita de categorias para a loja

--- src/main/java/br/com/alura/modelo/Categoria.java
+++ src/main/java/br/com/alura/modelo/Categoria.java
@@ -1,8 +1,30 @@
 package br.com.alura.modelo;
 
-public enum Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-	INFORMATICA,
-	LIVROS,
-	CELULARES;
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	
+	public Categoria() {
+		
+	}
+	
+	public Categoria(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
 }
