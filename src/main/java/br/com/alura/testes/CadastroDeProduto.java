4028d3e420852c204db5468dc4d03c6119ba8cc7
AliceMeneses <alicesilvademeneses@gmail.com>
2021-05-01 17:47:03 -0300

    Adicionar busca por id e buscar todos os produtos

--- src/main/java/br/com/alura/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/testes/CadastroDeProduto.java
@@ -1,6 +1,7 @@
 package br.com.alura.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -14,6 +15,36 @@ public class CadastroDeProduto {
 
 	public static void main(String[] args) {
 
+		cadastrarProduto();
+		
+		buscarProduto();
+		
+		buscarTodosProdutos();
+	}
+	
+	private static void buscarTodosProdutos() {
+		
+		EntityManager entityManager = JPAUtil.getEntityManager();
+		
+		ProdutoDao produtoDao = new ProdutoDao(entityManager);
+				
+		List<Produto> produtos = produtoDao.buscarTodos();
+		
+		produtos.forEach(System.out::println);
+	}
+
+	private static void buscarProduto() {
+		
+		EntityManager entityManager = JPAUtil.getEntityManager();
+				
+		ProdutoDao produtoDao = new ProdutoDao(entityManager);
+				
+		Produto produto = produtoDao.buscarPorId(1L);
+		
+		System.out.println(produto);
+	}
+
+	private static void cadastrarProduto() {
 		EntityManager entityManager = JPAUtil.getEntityManager();
 		
 		Categoria celulares = new Categoria("CELULARES");
