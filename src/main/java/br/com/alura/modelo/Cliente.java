30c656d0a5c845d5da8a07e29fa2e380604f931d
AliceMeneses <alicesilvademeneses@gmail.com>
2021-05-02 16:50:03 -0300

    Adicionar getters e setters para os atributos

--- src/main/java/br/com/alura/modelo/Cliente.java
+++ src/main/java/br/com/alura/modelo/Cliente.java
@@ -24,6 +24,30 @@ public class Cliente {
 		this.nome = nome;
 		this.cpf = cpf;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 	
 	
 }
