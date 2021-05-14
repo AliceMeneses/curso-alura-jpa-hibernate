48551eda5ba8d4125cde1424293ea6017e2d65ea
AliceMeneses <alicesilvademeneses@gmail.com>
2021-05-14 09:45:23 -0300

    Adicionar chave composta CategoriaId para Categoria

--- src/main/java/br/com/alura/modelo/Categoria.java
+++ src/main/java/br/com/alura/modelo/Categoria.java
@@ -1,30 +1,28 @@
 package br.com.alura.modelo;
 
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "categorias")
 public class Categoria {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String nome;
 	
-	public Categoria() {
-		
+	@EmbeddedId
+	private CategoriaId id;
+	
+	public Categoria() {		
 	}
 	
-	public Categoria(String nome) {
-		this.nome = nome;
+	public Categoria(String nome, String tipo){
+		id = new CategoriaId(nome, tipo);
 	}
 	
-	public String getNome() {
-		return nome;
+	public String getNome(){
+		return this.id.getNome();
 	}
 	
+	public String getTipo() {
+		return this.id.getTipo();
+	}
 }
